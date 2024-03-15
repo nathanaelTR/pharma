8bfe78187cf2cc22216a18d0bf3f93bd2a37bd24
tasnim <tasnim.alioui@ltpdampierre.fr>
2024-03-15 14:12:44 +0100

    visiteur config

---
+++ src/main/java/fr/app/visiteur.java
@@ -0,0 +1,72 @@
+package fr.app;
+
+public class visiteur {
+    package fr.app;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class interactionController {
+
+    @FXML
+    private TextField Dateinput;
+
+    @FXML
+    private Button Envoyer;
+
+    @FXML
+    private Label KilometrageTotal;
+
+    @FXML
+    private TextField Kilometrageinput;
+
+    @FXML
+    private TextField Libelléinput;
+
+    @FXML
+    private TextField Matriculeinput;
+
+    @FXML
+    private TextField Moisinput;
+
+    @FXML
+    private TextField Montantinput;
+
+    @FXML
+    private TextField Nuitinput;
+
+    @FXML
+    private TextField Repasmidiinput;
+
+    @FXML
+    private Label TotalNuit;
+
+    @FXML
+    private Label TotalRepasmidi;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private TextField user_poste;
+
+    @FXML
+    private TextField user_session;
+
+    @FXML
+    void deconnexion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userSession(ActionEvent event) {
+
+    }
+
+}
+
+    
+}
