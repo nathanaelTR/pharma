5c67eb6b3751c31641ca54ab823b88380d182152
nathanaelTR <nathanael.trt@outlook.fr>
2024-03-29 16:53:17 +0100

    Update Historique + Changement de nom

--- src/main/java/fr/app/historiqueController.java
+++ src/main/java/fr/app/historiqueController.java
@@ -1,11 +1,26 @@
 package fr.app;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
 public class historiqueController {
 
-    @FXML
+       @FXML
     private ListView<?> Historique_affichage;
+
+    @FXML
+    void Retour(ActionEvent event) throws IOException {
+        App.setRoot("application_visiteur");
+    }
+
+    @FXML
+    void deconnexion(ActionEvent event) throws IOException {
+        App.setRoot("connexion");
+        connexionController.UtilisateurConnecter = 0;
+    }
+
 
 }
