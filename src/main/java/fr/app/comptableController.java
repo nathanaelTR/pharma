cca47c4cad9ae4b4a78219f64f52c8293b155974
nathanaelTR <nathanael.trt@outlook.fr>
2024-03-29 14:30:51 +0100

    Interface Comptable Update

---
+++ src/main/java/fr/app/comptableController.java
@@ -0,0 +1,16 @@
+package fr.app;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class comptableController {
+    
+    @FXML
+    void deconnexion(ActionEvent event) throws IOException {
+
+        App.setRoot("connexion");
+
+    }
+}
