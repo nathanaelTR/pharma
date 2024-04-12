c26d729f4e1316487ef8c20d68710fa1f24842b6
nathanaelTR <nathanael.trt@outlook.fr>
2024-04-12 10:51:17 +0200

    Update Historique

--- src/main/java/fr/app/historiqueController.java
+++ src/main/java/fr/app/historiqueController.java
@@ -2,18 +2,56 @@ package fr.app;
 
 import java.io.IOException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
+import java.sql.*;
 
 public class historiqueController {
 
-       @FXML
-    private ListView<?> Historique_affichage;
+    ObservableList<String> visiteurFicheDeFrais = FXCollections.observableArrayList();
+
+    @FXML
+    private ListView<String> Historique_affichage;
+
+    @FXML
+    private DatePicker DateGet;
 
     @FXML
     void Retour(ActionEvent event) throws IOException {
         App.setRoot("application_visiteur");
+    }
+
+    @FXML
+    void initialize() throws SQLException {
+
+        String sql = "SELECT f.id, f.id_visiteur ,f.date_DeCreation FROM fiche_de_frais f INNER JOIN utilisateur u ON u.id = f.id_visiteur INNER JOIN etat e ON e.id = f.fk_etat" +
+                " WHERE u.id = '" + connexionController.getUser() + "';";
+
+        
+        Connect connect = new Connect();
+        Connection conn = DriverManager.getConnection(connect.dbURL, connect.nomUtilisateur, connect.mdp);
+
+        Statement requete = conn.createStatement();
+        ResultSet res = requete.executeQuery(sql);
+    
+
+        while (res.next()) {
+            int IdFiche = res.getInt("id");
+            String dateCreation = res.getString("date_DeCreation");
+            
+            String sortie = "Fiche ID: " + IdFiche + " Date : " + dateCreation;
+            visiteurFicheDeFrais.add(sortie);
+        };
+        Historique_affichage.setItems(visiteurFicheDeFrais);
+    
+    }
+
+    private void getPicker() {
+        
     }
 
     @FXML
@@ -23,3 +61,7 @@ public class historiqueController {
     }
 
 }
+
+
+     //   sql = "SELECT f.id, f.id_visiteur ,f.date_DeCreation FROM fiche_de_frais f INNER JOIN utilisateur u ON u.id = f.id_visiteur INNER JOIN etat e ON e.id = f.fk_etat" +
+        // " WHERE u.id = '" + connexionController.getUser() + "' AND MONTH(f.date_DeCreation) = '" + DateGet.getValue().getMonthValue() + "';";
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module fr.app {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.base;
 
     opens fr.app to javafx.fxml;
     exports fr.app;
